8914906736f0d1610a231b655a6ac02e169d0eeb
Philipp Marock <44873758+Sahibabdul@users.noreply.github.com>
2022-05-03 19:37:57 +0200

    problem with ads and more articles  for testing and small bugfix

--- 20min/20min_main.py
+++ 20min/20min_main.py
@@ -5,10 +5,10 @@ import time
 from xml.dom import minidom 
 
 
-urls = ["https://www.20min.ch/story/bei-lex-netflix-wird-es-knapp-695939832762", "https://www.20min.ch/story/buergerliche-bekaempfen-stimmrechtsalter-16-mit-fake-statements-490546418948", "https://www.20min.ch/story/cervelat-werbeverbot-hat-keine-chance-485954729793"]
+urls = ["https://www.20min.ch/story/bei-lex-netflix-wird-es-knapp-695939832762", "https://www.20min.ch/story/buergerliche-bekaempfen-stimmrechtsalter-16-mit-fake-statements-490546418948", "https://www.20min.ch/story/cervelat-werbeverbot-hat-keine-chance-485954729793", "https://www.20min.ch/story/darum-geht-es-bei-der-frontex-vorlage-486320411249", "https://www.20min.ch/story/gescheitertes-mediengesetz-gibt-erneuter-attacke-auf-srg-aufwind-741839757365", "https://www.20min.ch/story/aargauer-verleger-will-plakat-von-gegnern-verbieten-597445079291"]
 
 def scroll_to_EOP(driver):
-    SCROLL_PAUSE_TIME = 0.5
+    SCROLL_PAUSE_TIME = 1.5
 
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -33,24 +33,31 @@ def get_comments_from_url(url):
     driver = webdriver.Chrome(r'C:\Users\PhilippMarock\Downloads\chromedriver_win32\chromedriver.exe', chrome_options=chrome_options)
     driver.get("https://www.20min.ch/comment/" + str(url.split("-")[-1]))
     driver.find_element_by_id("onetrust-accept-btn-handler").click()
-    scroll_to_EOP(driver)
-    comments = driver.find_element_by_id("commentSection").text
     time.sleep(2)
+    scroll_to_EOP(driver)
+    time.sleep(2)
+    comments = driver.find_element_by_id("commentSection").text
     driver.close()
     comments = comments.split("\n")[5:]
     print(len(comments))
     return comments
 
-with open("comments_20min.xml", "w", encoding="utf-8") as writer:
-    comments = get_comments_from_url(urls[0])
+articles = {}
+xml_str = ""
+for url in urls:
+    articles[url] = get_comments_from_url(url)
+for item in articles.keys():
+    comments = articles[item]    
     comments = "\n".join(comments)
+    comments = comments.replace("Werbung\n", "")
     comments = comments.split("Kommentar melden\n")
     comments = comments[:-1]
     root = minidom.Document()
-    xml = root.createElement("Netflix")
+    xml = root.createElement(item)
     root.appendChild(xml)
     for comment in comments:
-        comment = comment.split("\n")
+        comment = comment.split("\n")[:-1]
+        print(comment[4])
         print(comment)
         productChild = root.createElement("Comment")
         productChild.setAttribute("author", comment[0])
@@ -60,6 +67,7 @@ with open("comments_20min.xml", "w", encoding="utf-8") as writer:
         productChild.setAttribute("main_reaction", comment[4].split("(")[0])
         productChild.setAttribute("main_reaction_amount", comment[4].split("(")[1].replace(" Lesende)", ""))
         xml.appendChild(productChild)
-    xml_str = root.toprettyxml(indent ="\t") 
-    with open("comments_20min.xml", "w", encoding="utf-8") as files:
-        files.write(xml_str)
+    xml_str = xml_str + root.toprettyxml(indent ="\t")
+xml_str = xml_str.replace("<?xml version=\"1.0\" ?>\n", "")
+with open("comments_20min.xml", "w", encoding="utf-8") as files:
+    files.write(xml_str)
