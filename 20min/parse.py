06048ed8d9a62804af277c6e9d126eaa8305db48
Philipp Marock <44873758+Sahibabdul@users.noreply.github.com>
2022-05-25 23:21:33 +0200

    only initiatives

--- 20min/parse.py
+++
@@ -1,1 +0,0 @@
-from germansentiment import SentimentModel
---
+++ LuzernerZeitung/sort_comments.py
@@ -0,0 +1,18 @@
+import csv
+
+file_name = "LuzernerZeitung/comments.xmlSoComp-Analysis.csv"
+rows = []
+with open(file_name, "r", encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        rows.append(row)
+
+words = ["initiative", "abstimmung", "gesetz", "kampfflugzeug", "kinderdrittbetreuungskosten", "begrenzungsinitiative", "zuwanderung", "kriegsmaterial", "verhüllungsverbot", " ehe ", "covid", "corona", "pflegeinitiative", "transplantationsgesetz", "filmgesetz"]
+
+out = []
+with open("LuzernerZeitung/onlyInitiative.csv", "w", encoding="utf-8") as csv_file:
+    writer = csv.writer(csv_file)
+    for row in rows:
+        if any(sub_str in row[0].lower() for sub_str in words):
+            out.append([row[0], row[1]])
+    writer.writerows(out)
